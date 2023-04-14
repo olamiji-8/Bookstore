680c856eb9416179a5637f87e7d5f57f83c7bd0e
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 18:31:34 +0100

    update

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { AppBar } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 
 const Header = () => {
+    const [value, setValue] = useState();
   return (
     <div>
-        <AppBar></AppBar>
+        <AppBar>
+            <Toolbar>
+            <Typography>
+                <LibraryBooksIcon/>
+            </Typography>
+            <Tabs 
+            textColor='inherit'
+            indicatorColor='secondary'
+            value={0}
+            >
+                <Tab label= "Add Product"/>
+                <Tab label= "About Us"/>
+            </Tabs>
+            </Toolbar>
+        </AppBar>
     </div>
   )
 }
