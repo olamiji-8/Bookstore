6ff757f818485418e9aa24aa2060e485984729e5
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 19:43:16 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import About from './Components/About'
+import AddBook from './Components/AddBook'
+import Book from './Components/Book/Book'
+import Books from './Components/Book/Books'
 import Header from './Components/Header'
+import Home from './Components/Home'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Routes>
-        <Route to="/" element ={<Home/>}/>
+        <Route path="/" element ={<Home/>} exact />
+        <Route path="/books" element ={<Books/>}/>
+        <Route path="/about" element ={<About/>}/>
+        <Route path ="/add" element ={<AddBook/>}/>
       </Routes>
     </div>
   )
