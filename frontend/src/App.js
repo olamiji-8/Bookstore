7bc4c05e3e703dae1b928bc78c43bb7aa850b73c
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 19:16:30 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Header from './Components/Header'
 
 const App = () => {
   return (
     <div>
       <Header/>
-      
+      <Routes>
+        <Route to="/" element ={<Home/>}/>
+      </Routes>
     </div>
   )
 }
