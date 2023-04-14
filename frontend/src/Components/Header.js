735b51a7c5184fcd8ea86179649e10b97fa71515
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 19:09:27 +0100

    update

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material'
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     const [value, setValue] = useState();
@@ -18,9 +19,9 @@ const Header = () => {
             value={value}
             onChange={(e, val)=>setValue(val)}
             >
-                <Tab label= "Add Product"/>
-                <Tab label= "Books"/>
-                <Tab label= "About Us"/>
+                <Tab LinkComponent={NavLink} to="/add" label= "Add Product"/>
+                <Tab LinkComponent={NavLink} to="/books"label= "Books"/>
+                <Tab LinkComponent={NavLink} to="/about" label= "About Us"/>
             </Tabs>
             </Toolbar>
         </AppBar>
