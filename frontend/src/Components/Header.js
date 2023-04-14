0266467508952abeb216f146d25d6b4c8cff4052
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 16:56:43 +0100

    update

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppBar } from '@mui/material'
+
+const Header = () => {
+  return (
+    <div>
+        <AppBar></AppBar>
+    </div>
+  )
+}
+
+export default Header
