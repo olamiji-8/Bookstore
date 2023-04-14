5a24ba453bb355040933e35b237d6b4bf673d5a2
olamiji-8 <olamijisotomi@gmail.com>
2023-04-14 14:10:57 +0100

    update

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const router = express.Router(); 
+const router = express.Router();
 const Book = require("../model/Book");
-const booksController = require("../controllers/books-controller")
+const booksController = require("../controllers/books-controller");
 
 router.get("/", booksController.getAllBooks);
-router.post("/", booksController.addBook)
-
+router.post("/", booksController.addBook);
+router.get("/:id", booksController.getById);
+router.put("/:id", booksController.updateBook);
+router.delete("/:id", booksController.deleteBook);
 
 module.exports = router;
