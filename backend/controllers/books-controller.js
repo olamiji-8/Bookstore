e27fb88244f885a83dd9b2c1ffffb8ff1171b77c
olamiji-8 <olamijisotomi@gmail.com>
2023-04-30 02:24:38 +0100

    update

--- backend/controllers/books-controller.js
+++ backend/controllers/books-controller.js
@@ -29,10 +29,9 @@ const getById = async (req, res, next) => {
 };
 
 const addBook = async (req, res, next) => {
-  const { name, author, description, price, available, image } = req.body;
-  let book;
   try {
-    book = new Book({
+    const { name, author, description, price, available, image } = req.body;
+    const book = new Book({
       name,
       author,
       description,
@@ -41,22 +40,18 @@ const addBook = async (req, res, next) => {
       image,
     });
     await book.save();
+    return res.status(201).json({ book });
   } catch (err) {
     console.log(err);
+    return res.status(500).json({ message: "Error adding book" });
   }
-
-  if (!book) {
-    return res.status(500).json({ message: "Unable To Add" });
-  }
-  return res.status(201).json({ book });
 };
 
 const updateBook = async (req, res, next) => {
-  const id = req.params.id;
-  const { name, author, description, price, available, image } = req.body;
-  let book;
   try {
-    book = await Book.findByIdAndUpdate(id, {
+    const id = req.params.id;
+    const { name, author, description, price, available, image } = req.body;
+    let book = await Book.findByIdAndUpdate(id, {
       name,
       author,
       description,
@@ -65,28 +60,30 @@ const updateBook = async (req, res, next) => {
       image,
     });
     book = await book.save();
+    if (!book) {
+      return res.status(404).json({ message: "Unable To Update By this ID" });
+    }
+    return res.status(200).json({ book });
   } catch (err) {
     console.log(err);
+    return res.status(500).json({ message: "Error updating book" });
   }
-  if (!book) {
-    return res.status(404).json({ message: "Unable To Update By this ID" });
-  }
-  return res.status(200).json({ book });
 };
 
 const deleteBook = async (req, res, next) => {
-  const id = req.params.id;
-  let book;
   try {
-    book = await Book.findByIdAndRemove(id);
+    const id = req.params.id;
+    const book = await Book.findByIdAndRemove(id);
+    if (!book) {
+      return res.status(404).json({ message: "Unable To Delete By this ID" });
+    }
+    return res.status(200).json({ message: "Product Successfully Deleted" });
   } catch (err) {
     console.log(err);
+    return res.status(500).json({ message: "Error deleting book" });
   }
-  if (!book) {
-    return res.status(404).json({ message: "Unable To Delete By this ID" });
-  }
-  return res.status(200).json({ message: "Product Successfully Deleted" });
 };
+
 
 exports.getAllBooks = getAllBooks;
 exports.addBook = addBook;
