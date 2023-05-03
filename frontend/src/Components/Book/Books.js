69bc4439f05c6ade920e60393e0c96395e28541f
olamiji-8 <olamijisotomi@gmail.com>
2023-05-03 14:26:22 +0100

    updated

--- frontend/src/Components/Book/Books.js
+++ frontend/src/Components/Book/Books.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+const URL = "http://localhost:3000/books";
 
+
+const fetchHandler =async ()=>{
+ return await axios.get(URL).then((res)=> res.data)
+}
 const Books = () => {
+  const [books, setBooks] = useState();
+  useEffect(()=>{
+    fetchHandler().then(data =>setBooks(data))
+  }, );
+
+console.log(books);
+
   return (
-    <div>Books</div>
+    <div>
+      Books
+    </div>
   )
 }
 
